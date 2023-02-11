20666ee7b203a2f11bc7e868b53fb0796853c304
jana <jantschick3@gmail.com>
2023-02-11 13:00:40 +0300

    Tregulov

---
+++ src/main/java/org/example/configuration/MySecurityInitializer.java
@@ -0,0 +1,8 @@
+package org.example.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+
+}
